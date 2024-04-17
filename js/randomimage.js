66bc60f48923623abcbc8dea16e070f011b9f48b
Ben Muth <ben.muth@web.de>
2024-04-18 02:04:01 +0300

    fix 3

--- js/randomimage.js
+++ js/randomimage.js
@@ -1,10 +1,11 @@
 const myButton = document.querySelector('button');
 const image = document.querySelector('img');
-const images = ['pexels-pixabay-259554.jpg','pexels-pixabay-37833.jpg','pexels-pixabay-47547.jpg','pexels-pixabay-53581.jpg'];
+const images = ['img/pexels-pixabay-259554.jpg','img/pexels-pixabay-37833.jpg','img/pexels-pixabay-47547.jpg','img/pexels-pixabay-53581.jpg'];
 
 myButton.addEventListener('click', changeImage);
 
 function changeImage() {
     let randomIndex = Math.floor(Math.random() * images.length);
     image.src = images[randomIndex];
+    console.log(randomIndex);
 }
